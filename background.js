da723639f16f0037a195859fb6cee79ddda9e274
Jonnyy <caijishiwo@gmail.com>
2019-12-17 23:43:48 -0500

    activate plugin on every tab update

---
+++ background.js
@@ -0,0 +1,9 @@
+//Active plugin on every tab update
+chrome.tabs.onUpdated.addListener(()=>{
+    chrome.tabs.query({active : true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { file : 'content.js'}
+        )
+    })
+})
