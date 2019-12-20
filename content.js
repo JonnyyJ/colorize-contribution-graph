adb01c6ef7d96c073504f672c96a8714853627ae
Jonnyy <caijishiwo@gmail.com>
2019-12-19 23:54:51 -0500

    made initColorize and colorizeActivety

--- content.js
+++ content.js
@@ -25,11 +25,29 @@ function initColorize()
 
 function colorizeActivity(color)
 {
+    const activity = document.querySelector('.js-highlight-blob')
+    if(activity)
+    {
+      const axes = document.querySelectorAll('.activity-overview-axis')
+      const ellipses = document.querySelectorAll('.activity-overview-point')
+      const blob = document.querySelector('.js-highlight-blob')
+      activity.setAttribute('stroke', color)
+      blob.style.fill = color
+      for(let ellipse of ellipses)
+        ellipse.style.stroke = color
+      for(let axis of axes)
+        axis.style.stroke = color
+    }
 
 }
 
 function colorizeLegend(mainColor, originalColors, legendItems)
 {
+  legendItems[0].setAttribute('style', `background-color:${originalColors[0]}`)
+  legendItems[1].setAttribute('style', `background-color:${colorLuminance(mainColor, 0.9)}`)
+  legendItems[2].setAttribute('style', `background-color:${colorLuminance(mainColor, 0.75)}`)
+  legendItems[3].setAttribute('style', `background-color:${colorLuminance(mainColor, 0.5)}`)
+  legendItems[4].setAttribute('style', `background-color:${mainColor}`)
 
 }
 
